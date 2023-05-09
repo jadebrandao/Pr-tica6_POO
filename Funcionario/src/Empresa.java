6d63984f31d8ea657e09341452ededf604cbc01c
Jade Brandão <108814737+jadebrandao@users.noreply.github.com>
2023-05-09 19:35:38 -0300

    Update Empresa.java

--- Funcionario/src/Empresa.java
+++ Funcionario/src/Empresa.java
@@ -1,11 +1,56 @@
-import java.util.ArrayList;
 public class Empresa {
+// e um array de objetos da classe Funcionario (o array pode armazenar até 100 funcionários).
+private String nomeEmpresa;
+private String cnpj;
+private int qtde_de_funcionario;
+Funcionario[] funcionarios;
+private static int count=0;
 
-    ArrayList<String> agenda = new ArrayList();
+public Empresa(String nomeEmpresa, String cnpj, int qtde_de_funcionario) {
+	this.nomeEmpresa = nomeEmpresa;
+	this.cnpj = cnpj;
+	if (qtde_de_funcionario<=100)
+	{
+		this.qtde_de_funcionario = qtde_de_funcionario;
+		funcionarios = new Funcionario[qtde_de_funcionario];
+	}
+	else
+	{
+		this.qtde_de_funcionario = 100;
+		funcionarios = new Funcionario[100];
+	}
+	
+}
 
-    int[] novo_array = new int[100];
-    private String nome;
-    private String cpf;
-    private int qtdFucionario;
+public String getNomeEmpresa() {
+	return nomeEmpresa;
+}
+
+public void setNomeEmpresa(String nomeEmpresa) {
+	this.nomeEmpresa = nomeEmpresa;
+}
+
+public String getCnpj() {
+	return cnpj;
+}
+
+public void setCnpj(String cnpj) {
+	this.cnpj = cnpj;
+}
+
+public int getQtde_de_funcionario() {
+	return qtde_de_funcionario;
+}
+
+public void setQtde_de_funcionario(int qtde_de_funcionario) {
+	this.qtde_de_funcionario = qtde_de_funcionario;
+}
+
+public void insereFuncionario(Funcionario func) {
+	funcionarios[count]=func;
+	count++;
+}
+
+
 
 }
